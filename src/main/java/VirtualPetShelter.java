fa7d9a40c8d92d2c7efdd837819ba0b48182171f
namilan <nichole.milan@gmail.com>
2019-05-21 16:20:53 -0400

    added more to tests for VPS

--- src/main/java/VirtualPetShelter.java
+++ src/main/java/VirtualPetShelter.java
@@ -26,8 +26,39 @@ public class VirtualPetShelter {
 	public void isHungry(VirtualPet pet) {
 		for (VirtualPet currentPet: virtualPets()) {
 			currentPet.isHungry();
+		}
 	}
 		
+	public void isThirsty(VirtualPet pet) {
+		for (VirtualPet currentPet: virtualPets()) {
+			currentPet.isThirsty();
+		}
 	}
+	
+	public void isNeedingPlay(VirtualPet pet) {
+		for (VirtualPet currentPet: virtualPets()) {
+			currentPet.isNeedingPlay();
+		}
+		
+	}
+	
+	public void isClean(VirtualPet pet) {
+		for (VirtualPet currentPet: virtualPets()) {
+			currentPet.isClean();
+		}
+	}
+	
+	public void isBored(VirtualPet pet) {
+		for (VirtualPet currentPet: virtualPets ()) {
+			currentPet.isVeryBored();
+		}
+	}
+	
+	public void isPetClean(VirtualPet pet) {
+		for (VirtualPet currentPet: virtualPets()) {
+			currentPet.isClean();
+		}
+	}
+		
 
 }
--- src/test/java/VirtualPetShelterTest.java
+++ src/test/java/VirtualPetShelterTest.java
@@ -4,6 +4,17 @@ import org.junit.Test;
 
 
 public class VirtualPetShelterTest {
+	
+	protected String name = "";
+	protected String type = "";
+	protected int health = 50;	
+	protected int hunger = 50;
+	protected int thirst = 50;
+	protected int play = 50;
+	protected int bored = 50;
+	protected int clean = 100;
+	protected int adopt = 1;
+	protected int admit = 1;
 
 	@Test
 	public void shouldReturnPetName() {
@@ -49,8 +60,60 @@ public class VirtualPetShelterTest {
 	}
 
 	private Object isHungry() {
-		// TODO Auto-generated method stub
 		return 50;
 	}
+	
+	@Test
+	public void shouldWaterPet() {
+		VirtualPet underTest = new VirtualPet("", "", 50);
+		int isThirsty = underTest.thirst = 50;
+		assertEquals(50, isThirsty());
+	}
+	
+	private Object isThirsty() {
+		return 50;
+	}
+	
+	@Test
+	public void shouldPlayWithPet() {
+		VirtualPet underTest = new VirtualPet("", "", 50);
+		int isNeedingPlay = underTest.play = 50;
+		assertEquals(50, isNeedingPlay());
+	}
+	
+	private Object isNeedingPlay() {
+		return 50;
+	}
+	
+	@Test
+	public void shouldCleanPet() {
+		VirtualPet underTest = new VirtualPet("", "", 50);
+		int isClean = underTest.clean = 50;
+		assertEquals(50, isClean());
+	}
+	
+	private Object isClean() {
+		return 50;
+	}
+	
+	@Test
+	public void isVeryBored() {
+		VirtualPet underTest = new VirtualPet("", "", 50);
+		int isVeryBored = underTest.bored = 50;
+		assertEquals(50, isBored());
+		
+	}
+	
+	private Object isBored() {
+		return 50;
+	}
+	
+	@Test
+	public void isPetClean() {
+		VirtualPet underTest = new VirtualPet("", "", 50);
+		int isClean = underTest.clean = 50;
+		assertEquals(50, isClean());
+	}
+	
 	
 }
